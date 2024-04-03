08546f1df1412541f3b29c60e22b9e7ec99c54e8
AlexMlt29 <alexandre.millet29600@gmail.com>
2024-04-03 17:21:47 +0200

    Add Housing Sheet Page

--- p8-kasa/src/page/HomePage.jsx
+++ p8-kasa/src/page/HomePage.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import Header from '../components/Header';
-import Body from '../components/Body';
 import Footer from '../components/Footer';
+import logo from "../logo/source1.png";
 
 function HomePage() {
-  // Votre code pour HomePage ici
   return (
   <div>
     <Header />
-    <Body />
+    <body>
+      <section className="menu">
+        <div className="banner">
+            <img src={logo} alt="Côte rocheuse embrumée avec des vagues s'écrasant sur le rivage, entourée d'arbres résistants au vent" className="banner-image" />
+            <div className="dark-background"></div>
+            <h1 className="banner-text">Chez vous, partout et ailleurs</h1>
+        </div>
+        <div className="cards">
+            <a href="/HousingSheet" className="cards-container">
+                <h2 className="cards-text">Titre de la location</h2>
+            </a>
+            <a href="/HousingSheet" className="cards-container">
+                <h2 className="cards-text">Titre de la location</h2>
+            </a>
+            <a href="/HousingSheet" className="cards-container">
+                <h2 className="cards-text">Titre de la location</h2>
+            </a>
+        </div>
+      </section>
+    </body>
     <Footer />
   </div>
   );
--- p8-kasa/src/page/HousingSheet.jsx
+++ p8-kasa/src/page/HousingSheet.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import logo from '../logo/SmallHouse.png';
+import staron from '../logo/star-on.png';
+import staroff from '../logo/star-off.png';
+import circle from '../logo/Host.png';
+import vector from '../logo/Vector.png';
 
 function HousingSheet() {
-  // Votre code pour HomePage ici
-  return <div>Fiche Logement Page Content</div>;
+  return (
+  <div>
+    <Header />
+    <body>
+      <section className="house">
+        <img src={logo} alt="Intérieur d'un salon lumineux, comportant une table à manger, une commode en bois. Une œuvre d'art colorée est accrochée au-dessus de la commode." />
+        <h2 className="house-title">Paris Center, on the romantic Canal Saint-Martin</h2>
+        <p className="house-text">Paris, Île-de-France</p>
+        <div className="tag-container">
+          <div className="house-tag"><h3 className="tag-text">Cozy</h3></div>
+          <div className="house-tag"><h3 className="tag-text">Canal</h3></div>
+          <div className="house-tag"><h3 className="tag-text">Paris 10</h3></div>
+        </div>
+        <div className="rating">
+          <div className="stars">
+            <img src={staron} />
+            <img src={staron} />
+            <img src={staron} />
+            <img src={staroff} />
+            <img src={staroff} />
+          </div>
+          <div className="rating-content">
+            <p className="rating-text">Alexandre Dumas</p>
+            <img src={circle} className="rating-circle" />
+          </div>
+        </div>
+        <div className="description">
+          <div className="rollmenu">
+            <p className="rollmenu-text">Description</p>
+            <img src={vector} className="rollmenu-image"/>
+          </div>
+          <div className="rollmenu">
+            <p className="rollmenu-text">Équipements</p>
+            <img src={vector} className="rollmenu-image"/>
+          </div>
+        </div>
+      </section>
+    </body>
+    <Footer />
+  </div>
+  );
 }
 
 export default HousingSheet;
